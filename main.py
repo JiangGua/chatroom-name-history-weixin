1a260e34c68e683c11b0bff91e6011b67c6f4b05
JiangGua <jonbgua@gmail.com>
2019-10-18 22:34:38 +0800

    将 锚点词提取法 函数化

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import itchat
 
+def anchor_in_chatroom_name(mark):
+    """
+        通过群聊名中手动设置的锚点词对群聊进行定位.
+        形参 mark 是一个字符串, 即手动设置保持不变的锚点, 
+        返回一个字符串, 为提取出的群名称。
+    """
+    chat_rooms = str(itchat.get_chatrooms())
+    end = chat_rooms.find(mark) + len(mark)
+    start = chat_rooms.rfind('NickName', 0, end) + 12
+    room_name = chat_rooms[start:end]
+    return room_name
+
 if __name__ == "__main__":
     itchat.auto_login(hotReload=True)
 
-    chat_rooms = str(itchat.get_chatrooms())
-    if len(chat_rooms) > 0:
-        with open('chatroom.json', 'w', encoding='utf-8') as file_obj:
-            file_obj.write(chat_rooms)
-
-    end = chat_rooms.find('[NSFW]') + 6
-    print(end)
-    start = chat_rooms.rfind('NickName', 0, end) + 12
-    print(start)
-
-    room_name = chat_rooms[start:end]
+    room_name = anchor_in_chatroom_name('[NSFW]')
     with open('a.txt', 'w', encoding='utf-8') as file_obj:
         file_obj.write(room_name)
