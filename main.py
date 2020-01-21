ebf7dd5dccc9c9a7f8ef9482937ab2d5a70f57e3
JiangGua <jonbgua@gmail.com>
2020-01-21 11:41:35 +0800

    保存群名模块

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import itchat
 from jinja2 import Environment, FileSystemLoader
 
@@ -29,9 +30,13 @@ def fetch_system_notification_name_change(msg):
         msg_memberlist = msg['User']['MemberList']
         current_member_list = set([str(item['NickName']) for item in msg_memberlist] + [str(item['DisplayName']) for item in msg_memberlist])
         
-        stored_member_list = get_stored_member_list()
-        #TODO: 那我要是第一次用，还没这个文件呢
-        
+        try:
+            stored_member_list = get_stored_member_list()
+        except IOError:
+            save_users(current_member_list)
+            save_chatroom_name(str(msg['Text'])[7:-1])
+            print(str(msg['Text'])[7:-1])
+
         if users_in_chatroom(current_member_list, stored_member_list) > (len(current_member_list) // 3):  # 允许 2/3 的用户改名（这么说其实并不严谨，毕竟有俩名）
             save_users(current_member_list)
             save_chatroom_name(str(msg['Text'])[7:-1])
@@ -51,15 +56,33 @@ def save_users(current_member_list):
         f.write(json.dumps(list(current_member_list)))
 
 def save_chatroom_name(name):
-    pass
+    def get_stored_history():
+        with open('output/name_history.json', 'r', encoding='utf-8') as f:
+            history = f.read()
+            history = json.loads(history)
+        return history
+
+    def save_history(history):
+         with open('output/name_history.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(list(history)))
+    try:
+        history = list(get_stored_history())
+    except IOError:
+        history = []
+
+    history.insert(0, {
+        "date": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+        "name": name,
+    })
+    save_history(history)
     
 def generate_timeline_webpage():
     root = os.path.dirname(os.path.abspath(__file__))
     templates_dir = os.path.join(root, 'static', 'template')
-    env = Environment( loader = FileSystemLoader(templates_dir) )
+    env = Environment(loader = FileSystemLoader(templates_dir))
     template = env.get_template('index.html')
     filename = os.path.join(root, 'static', 'html', 'index.html')
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         f.write(template.render(
             title = "Hello Jinja2",
             items    = [{
@@ -74,6 +97,5 @@ def generate_timeline_webpage():
 if __name__ == "__main__":
     if not os.path.exists('output'):
         os.makedirs('output')
-    generate_timeline_webpage()
     itchat.auto_login(hotReload=True)   # enableCmdQR=2
     itchat.run()
