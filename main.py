b392249e63ac75caf734596588153a19f29ced58
JiangGua <jonbgua@gmail.com>
2020-01-22 18:06:24 +0800

    FIX: 自动创建html_dir

--- main.py
+++ main.py
@@ -19,13 +19,18 @@ def generate_timeline_webpage():
     templates_dir = os.path.join(root, 'static', 'template')
     env = Environment(loader = FileSystemLoader(templates_dir))
     template = env.get_template('index.html')
-    filename = os.path.join(root, 'static', 'html', 'index.html')
+    html_dir = os.path.join(root, 'static', 'html')
+    filename = os.path.join(html_dir, 'index.html')
     config = os.path.join(root, 'config.json')
     with open(config, 'r',encoding='utf-8') as f:
         config = f.read()
         config = json.loads(config)
         title = str(config["title"])
     history = get_stored_history()
+
+    if not os.path.exists(html_dir):
+        os.makedirs(html_dir)
+
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(template.render(
             title = title,
